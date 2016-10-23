593f3c45ebdc5e51ae7a9074095f521ded4ce9d1
Wesley Ellis <tahnok@gmail.com>
2016-10-23 18:56:25 -0400

    make alarm activity make noise

--- app/src/main/java/me/tahnok/wesclock/AlarmActivity.java
+++ app/src/main/java/me/tahnok/wesclock/AlarmActivity.java
@@ -1,18 +1,33 @@
 package me.tahnok.wesclock;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class AlarmActivity extends AppCompatActivity {
 
+    protected Ringtone ringtone;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ringtone.stop();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
         ButterKnife.bind(this);
+
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        ringtone = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        ringtone.play();
     }
 
     @OnClick(R.id.exit_button)
--- app/src/main/java/me/tahnok/wesclock/SetAlarmActivity.java
+++ app/src/main/java/me/tahnok/wesclock/SetAlarmActivity.java
@@ -27,13 +27,10 @@ public class SetAlarmActivity extends AppCompatActivity implements TimePickerDia
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.test_alarm) {
+            testAlarm();
             return true;
         }
 
@@ -70,7 +67,6 @@ public class SetAlarmActivity extends AppCompatActivity implements TimePickerDia
         render();
     }
 
-    @OnClick(R.id.test_button)
     public void testAlarm() {
         Intent intent = new Intent(this, AlarmActivity.class);
         startActivity(intent);
