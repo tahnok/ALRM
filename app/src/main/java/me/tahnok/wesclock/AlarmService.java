0eac56e9c814a8a8c79fc8dfdf1e7c1cb9a028c1
Wesley Ellis <tahnok@gmail.com>
2016-10-24 20:20:55 -0400

    remove looper

--- app/src/main/java/me/tahnok/wesclock/AlarmService.java
+++ app/src/main/java/me/tahnok/wesclock/AlarmService.java
@@ -8,62 +8,24 @@ import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
 import android.os.Process;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
-
-/**
- * Created by wes on 2016-10-23.
- */
 
 public class AlarmService extends Service {
     private static final int START_ALARM = 101;
     private static final int STOP_ALARM = 102;
     public static final String EXTRA_COMMAND = "command";
 
-    private ServiceHandler serviceHandler;
     private Ringtone ringtone;
-
-    private final class ServiceHandler extends Handler {
-        public ServiceHandler(Looper looper) {
-            super(looper);
-        }
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.arg1) {
-                case START_ALARM:
-                    playAlarm();
-                    break;
-                case STOP_ALARM:
-                    stopAlarm();
-                    break;
-            }
-        }
-    }
-
-    public void playAlarm() {
-        ringtone.play();
-        Intent intent = new Intent(getApplicationContext(), AlarmActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-
-    public void stopAlarm() {
-        ringtone.stop();
-        Toast.makeText(getApplicationContext(), "Stopped", Toast.LENGTH_LONG).show();
-    }
 
     @Override
     public void onCreate() {
         HandlerThread thread = new HandlerThread("ServiceStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
         thread.start();
 
-        serviceHandler = new ServiceHandler(thread.getLooper());
         Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         ringtone = RingtoneManager.getRingtone(getApplicationContext(), notification);
     }
@@ -73,9 +35,17 @@ public class AlarmService extends Service {
         if (intent == null) {
             return START_NOT_STICKY;
         }
-        Message msg = serviceHandler.obtainMessage();
-        msg.arg1 = intent.getIntExtra(EXTRA_COMMAND, START_ALARM);
-        serviceHandler.sendMessage(msg);
+        int command = intent.getIntExtra(EXTRA_COMMAND, START_ALARM);
+        switch (command) {
+            case START_ALARM:
+                playAlarm();
+                break;
+            case STOP_ALARM:
+                stopAlarm();
+                break;
+            default:
+                Toast.makeText(getApplicationContext(), "??????????", Toast.LENGTH_LONG).show();
+        }
 
         return START_STICKY;
     }
@@ -84,6 +54,18 @@ public class AlarmService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    protected void playAlarm() {
+        ringtone.play();
+        Intent intent = new Intent(getApplicationContext(), AlarmActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    protected void stopAlarm() {
+        ringtone.stop();
+        Toast.makeText(getApplicationContext(), "Stopped", Toast.LENGTH_LONG).show();
     }
 
     public static void scheduleAlarm(Context context, Time time) {
