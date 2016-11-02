e8f1a941d597a13aefcde8d6ab9b69d9d2998c80
Wesley Ellis <tahnok@gmail.com>
2016-11-01 21:38:24 -0400

    launch activity over lockscreen and when off

--- app/src/main/java/me/tahnok/wesclock/AlarmActivity.java
+++ app/src/main/java/me/tahnok/wesclock/AlarmActivity.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -14,6 +15,14 @@ public class AlarmActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                                 | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                                 | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                                 | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+                                 | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+
+
         setContentView(R.layout.activity_alarm);
         ButterKnife.bind(this);
     }
